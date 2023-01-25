8e3243a7bf5ab6d8c7cf3510e2abe6411dc94141
stevemoore9595 <stevemoore9595@gmail.com>
2023-01-24 19:38:51 -0700

    added the body, header, section 1, and search history styleing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=ogden&appid=9859fc6998842f2d4d3f91cde44162d0').then(function(response){
+fetch('https://api.openweathermap.org/data/2.5/weather?q=ogden&appid=d02f6a61ab39731d12f55d46b13ebb91').then(function(response){
     return response.json()
 }).then(function(data){
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=41.223&lon=-111.9738&appid=9859fc6998842f2d4d3f91cde44162d0').then(function(response){
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=41.223&lon=-111.9738&appid=d02f6a61ab39731d12f55d46b13ebb91').then(function(response){
         return response.json()
     }).then(function(data){
 console.log(data)
